3a72970321b0985710070dea40855cf04963a307
edrel <edrell43@gmail.com>
2025-04-20 04:03:22 +0800

    Update image remote patterns for production and development environments

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "192.168.1.10",
-        port: "5001",
+        protocol: "https",
+        hostname: isProduction ? "learning-u25a.onrender.com" : "192.168.1.10",
         pathname: "/uploads/**",
       },
     ],
