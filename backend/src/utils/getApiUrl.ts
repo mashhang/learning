7d4a03127102a14c90fbc9b913cb5233bb659f65
edrel <edrell43@gmail.com>
2025-04-24 17:46:41 +0800

    Fix API URL logic to use production URL when USE_LOCAL_API is set to false

--- backend/src/utils/getApiUrl.ts
+++ backend/src/utils/getApiUrl.ts
@@ -1,4 +1,4 @@
 export const getApiUrl = () => {
-  const useLocal = process.env.USE_LOCAL_API === "true";
+  const useLocal = process.env.USE_LOCAL_API === "false";
   return useLocal ? process.env.LOCAL_API_URL : process.env.PROD_API_URL;
 };
